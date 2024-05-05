a1f57d81cdb30f7ad0fa38fa9380f854477a814b
MarwaAshraf1812 <marashraf090@gmail.com>
2024-05-05 23:06:33 +0300

    4. Writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -15,3 +15,42 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+
+export interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+export interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+export function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName[0]}. ${lastName}`
+}
+
+export interface StudentConstructor {
+  new(firstName: string, lastName: string): IsStudentClass;
+}
+
+export interface IsStudentClass {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+export class StudentClass implements IsStudentClass {
+  private _firstName: string;
+  private _lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+  }
+
+  workOnHomework() : string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this._firstName;
+  }
+}
