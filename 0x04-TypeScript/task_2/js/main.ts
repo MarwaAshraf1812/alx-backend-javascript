5bb24c9e898a95d0fa029cc291f3b58770d22524
MarwaAshraf1812 <marashraf090@gmail.com>
2024-05-05 23:30:37 +0300

    7. String literal types

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -46,3 +46,32 @@ export function createEmployee(salary: (number |string)): (Director | Teacher) {
     return new Director();
   }
 }
+
+export function isDirector(employee: (Director | Teacher)) {
+  if (employee instanceof Director) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+export function executeWork(employee: (Director | Teacher)) {
+  if (isDirector(employee)) {
+    return (employee as Director).workDirectorTasks();
+  }
+  else {
+    return (employee as Teacher).workTeacherTasks();
+  }
+}
+
+export type Subjects = 'Math' | 'History';
+
+export function teachClass(todayClass: Subjects) {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  }
+  if (todayClass === 'History') {
+    return 'Teaching History';
+  }
+}
