1c1e9d9862619c3082a25401cc09651f1967e4a8
MarwaAshraf1812 <marashraf090@gmail.com>
2024-06-19 00:39:03 +0300

    Create a more complex HTTP server using Express

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -10,13 +10,13 @@ app.get('/', (req, res) => {
 });
 
 app.get('/students', async (req, res) => {
-    const msg = "This is the list of our students\n";
-    try {
-        const students = await countStudents(DATABASE);
-        res.send(`${msg}${students.join('\n')}`);
-    } catch (error) {
-        res.send(`${msg}${error.message}`);
-    }
+  const msg = 'This is the list of our students\n';
+  try {
+    const students = await countStudents(DATABASE);
+    res.send(`${msg}${students.join('\n')}`);
+  } catch (error) {
+    res.send(`${msg}${error.message}`);
+  }
 });
 
 app.listen(1245, () => {
