2c831eea487572e82706417e34237bef81a2ed4c
MarwaAshraf1812 <marashraf090@gmail.com>
2024-06-18 22:10:41 +0300

    style fixed

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,5 @@
 /*  create a function named displayMessage
-that prints in STDOUT the string argument.*/
+that prints in STDOUT the string argument. */
 
 function displayMessage(message) {
   console.log(message);
